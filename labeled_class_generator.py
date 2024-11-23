950c3e2385a9612cb97a8aa4609c4e47cccf2c86
stekuz <stekuzand@gmail.com>
2024-11-23 11:31:09 +0300

    generator

--- labeled_class_generator.py
+++ labeled_class_generator.py
@@ -3,8 +3,10 @@ import json
 import random
 import requests
 
-#run server with:$ java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
-print(requests.post('http://[::]:9000/?properties={"annotators":"tokenize,pos","outputFormat":"json"}', data = {'data':'John hit the ball.'}).text)
+#run server with:
+# $ cd ~/Desktop/stanford-corenlp-4.5.7
+# $ java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+#print(requests.post('http://[::]:9000/?properties={"annotators":"parse","outputFormat":"json"}', data = {'data':'A strong man had hit the ball.'}).json()['sentences'][0]['parse'])
 
 def set_generator():
     output_json = open('./class_labeled/set.json','w')
@@ -99,3 +101,119 @@ def atom_generator():
         name += content
         res[name] = atom_object.__dict__
     json.dump(res,output_json)
+
+def random_tree_generator(n):
+    parents = [0]
+    number_of_children = [0]*n
+    for i in range(1,n):
+        parent = random.randint(0,i-1)
+        additional_child = random.randint(0,10)
+        while number_of_children[parent] >= 2 and additional_child > 2:
+            parent = random.randint(0,i-1)
+            additional_child = random.randint(0,10)
+        parents.append(parent)
+        number_of_children[parent] += 1
+    return parents
+
+def get_leaves(tree_parents):
+    n = len(tree_parents)
+    have_children = [0]*n
+    for i in range(n):
+        have_children[tree_parents[i]] = 1
+    leaves = []
+    for i in range(n):
+        if have_children[i] == 0:
+            leaves.append(i)
+    return leaves
+
+def action_generator():
+    output_json = open('./class_labeled/action.json','w')
+    parts_of_speech_tags = ['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
+    latin_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    latin_alphabet += latin_alphabet.upper()
+    latin_alphabet += '0123456789\\_-'
+    number_of_samples = 100
+    res = {}
+    for i in range(number_of_samples):
+        tree_parents = random_tree_generator(random.randint(5,50))
+        tree_children = []
+        n = len(tree_parents)
+        for j in range(n):
+            tree_children.append([])
+        for j in range(1,n):
+            tree_children[tree_parents[j]].append(j)
+        leaves = get_leaves(tree_parents)
+        tree_labels = ['']*n
+        for leave in leaves:
+            tree_labels[leave] = parts_of_speech_tags[random.randint(0,len(parts_of_speech_tags)-1)]
+        not_visited_children = [0]*n
+        for j in range(1,n):
+            not_visited_children[tree_parents[j]] += 1
+        def lift(v):
+            if v == 0:
+                tree_labels[v] = 'S'
+                return
+            if not_visited_children[v]:
+                return
+            for child in tree_children[v]:
+                if 'N' in tree_labels[child]:
+                    tree_labels[v] = 'NP'
+                    break
+                elif 'V' in tree_labels[child]:
+                    tree_labels[v] = 'VP'
+                    break
+                elif 'J' in tree_labels[child]:
+                    tree_labels[v] = 'JP'
+                    break
+                elif 'P' in tree_labels[child]:
+                    tree_labels[v] = 'PP'
+                    break
+                elif 'R' in tree_labels[child]:
+                    tree_labels[v] = 'RP'
+                    break
+                else:
+                    continue
+            if tree_labels[v] == '':
+                tree_labels[v] = tree_labels[tree_children[v][0]]
+            not_visited_children[tree_parents[v]] -= 1
+            lift(tree_parents[v])
+        for leave in leaves:
+            not_visited_children[tree_parents[leave]] -= 1
+            lift(tree_parents[leave])
+        words = ['']*len(leaves)
+        for j in range(len(words)):
+            word = ''
+            wordlen = random.randint(1,15)
+            for jj in range(wordlen):
+                word += latin_alphabet[random.randint(0,len(latin_alphabet)-1)]
+            words[j] = word
+        text = ''
+        for word in words:
+            text += word + ' '
+        action_object = Action('','','')
+        last_noun = -1
+        actor_name_leave = -1
+        for j in range(len(leaves)):
+            if 'N' in tree_labels[leaves[j]]:
+                if last_noun == -1:
+                    last_noun = j
+                elif action_object.actor_name == '':
+                    for k in range(last_noun+1,j):
+                        if 'R' in tree_labels[leaves[k]]:
+                            action_object.actor_name = words[j]
+                            actor_name_leave = j
+                            break
+                        elif 'IN' == tree_labels[leaves[k]]:
+                            action_object.actor_name = words[last_noun]
+                            actor_name_leave = last_noun
+                            break
+            if 'V' in tree_labels[leaves[j]]:
+                action_object.output = words[j]
+        for j in range(len(leaves)):
+            if 'N' in tree_labels[leaves[j]]:
+                if actor_name_leave != j:
+                    action_object.input_list += words[j] + ' '
+        res[text] = action_object.__dict__
+    json.dump(res,output_json)
+
+action_generator()

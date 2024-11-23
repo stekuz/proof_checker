7360aceeb6b6189cebb23c7da5bd58ee97e30331
stekuz <stekuzand@gmail.com>
2024-11-23 11:49:16 +0300

    generator

---
+++ data_to_graph.py
@@ -0,0 +1,29 @@
+def atom_to_graph(key,value):
+    def json_record_to_text(key,value):
+        text = ''
+        text += key
+        text += '\\\\\\'
+        for key2 in value:
+            text += key2
+            text += '\\'
+            text += value[key2]
+            text += '\\\\'
+        return text 
+    separators = [' ',',','.',';',':','\\','\\\\','\\\\\\']
+    text = json_record_to_text(key,value)
+    adj_matrix = []
+    n = len(text)
+    for i in range(n):
+        adj_matrix.append([0]*n)
+    word_now = ''
+    for i in range(n-1):
+        if text[i+1] not in separators:
+            adj_matrix[i][i+1] = 1
+            word_now += text[i]
+        else:
+            index = i
+            while True:
+                index = text[index+1:].find(word_now)
+                if index == -1:
+                    break
+                adj_matrix[i][index] = 1
